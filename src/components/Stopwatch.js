c2adc854b1dda30dbca0f080bf89a688e31b9958
Chukwuebuka Chidi-Ezirim <chizim2003@gmail.com>
2024-10-04 11:57:09 +0100

    refactored my code and added some added functionality to display the time in the ui

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 const Stopwatch = () => {
-  const [elapsedTime, setElaspsedTime] = useState(0);
+  const [elapsedTime, setElapsedTime] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
 
   const startStopwatch = () => {
@@ -13,27 +13,37 @@ const Stopwatch = () => {
     setIsRunning(false);
   };
   const resetStopwatch = () => {
-    setElaspsedTime(0);
+    setElapsedTime(0);
     setIsRunning(false);
   };
 
   const formatTime = () => {
-    const hour = (elapsedTime / (1000 * 60 * 60))
-    const minute = (elapsedTime /(1000 * 60))
-    const second = (elapsedTime / 1000)
-  }
+    const hour = elapsedTime / (1000 * 60 * 60);
+    const minute = elapsedTime / (1000 * 60);
+    const second = elapsedTime / 1000;
+    return `${hour.toString().padStart(2, '0')}:${minute
+        .toString()
+        .padStart(2, '0')}:${second.toString().padStart(2, '0')}`
+    
+  };
   useEffect(() => {
+    let interval = 0;
     if (isRunning === true) {
-      setInterval(() => {
-        setElaspsedTime((prevTime) => prevTime + 1000);
-      }, 100);
-    }
+      interval = setInterval(() => {
+        setElapsedTime((prevTime) => prevTime + 1000);
+      }, 1000);
+    } else {
+    clearInterval(interval);
+    } 
+    return () => clearInterval(interval)
   }, [isRunning]);
+  
+  formatTime()
 
   return (
     <section>
       <h2>STOPWATCH</h2>
-      <div>{elapsedTime}</div>
+      <div>{formatTime}</div>
       <button onClick={startStopwatch}>Start</button>
       <button onClick={stopStopwatch}>Stop</button>
       <button onClick={resetStopwatch}>Reset</button>
