c0371a0dc33f266a1bde5720637940d2104baef9
Chukwuebuka Chidi-Ezirim <chizim2003@gmail.com>
2024-10-04 12:17:34 +0100

    added css styles to my app

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -18,32 +18,29 @@ const Stopwatch = () => {
   };
 
   const formatTime = () => {
-    const hour = elapsedTime / (1000 * 60 * 60);
-    const minute = elapsedTime / (1000 * 60);
-    const second = elapsedTime / 1000;
-    return `${hour.toString().padStart(2, '0')}:${minute
-        .toString()
-        .padStart(2, '0')}:${second.toString().padStart(2, '0')}`
-    
+    const hour = Math.floor(elapsedTime / (1000 * 60 * 60));
+    const minute = Math.floor(elapsedTime / (1000 * 60));
+    const second = Math.floor(elapsedTime / 1000);
+    return `${hour.toString().padStart(2, "0")}:${minute
+      .toString()
+      .padStart(2, "0")}:${second.toString().padStart(2, "0")}`;
   };
   useEffect(() => {
     let interval = 0;
     if (isRunning === true) {
       interval = setInterval(() => {
-        setElapsedTime((prevTime) => prevTime + 1000);
-      }, 1000);
+        setElapsedTime((prevTime) => prevTime + 100);
+      }, 100);
     } else {
-    clearInterval(interval);
-    } 
-    return () => clearInterval(interval)
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
   }, [isRunning]);
-  
-  formatTime()
 
   return (
     <section>
       <h2>STOPWATCH</h2>
-      <div>{formatTime}</div>
+      <div>{formatTime()}</div>
       <button onClick={startStopwatch}>Start</button>
       <button onClick={stopStopwatch}>Stop</button>
       <button onClick={resetStopwatch}>Reset</button>
